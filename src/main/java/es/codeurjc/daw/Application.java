d8ab7ba38dbae3422f420088a90a90c259042d8d
Juanmeta <juanmabarrio@gmail.com>
2020-02-23 10:47:31 +0100

    initial commit

---
+++ src/main/java/es/codeurjc/daw/Application.java
@@ -0,0 +1,21 @@
+package es.codeurjc.daw;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+
+@SpringBootApplication
+@EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", password="${cloud.aws.rds.springaws.password}")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
